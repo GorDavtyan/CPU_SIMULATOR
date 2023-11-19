b23db33777391850d9786487cf3941e6ce05620b
Gor Davtyan <gordavtyan705@gmail.com>
2023-11-19 23:52:12 +0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import os.OS;
+
+public class Main {
+    public static void main(String[] args) {
+        OS os = new OS();   // Create an instance of the OS class
+        os.execute("code.txt");  // Execute the OS by providing the file path of the source code
+    }
+}
